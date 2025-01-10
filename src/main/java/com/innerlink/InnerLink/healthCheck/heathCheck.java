004918289bc547492c1411ee11ebeb5c26ee5cdf
M4ster SINISTER <91175533+MasterSINISTER@users.noreply.github.com>
2025-01-10 11:38:07 +0530

    Added!
    Health Check to check Application is Working!!

---
+++ src/main/java/com/innerlink/InnerLink/healthCheck/heathCheck.java
@@ -0,0 +1,14 @@
+package com.innerlink.InnerLink.healthCheck;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class heathCheck {
+    @GetMapping("/health-check")
+    public String heathCheck(){
+        return "Working!";
+    }
+
+}
