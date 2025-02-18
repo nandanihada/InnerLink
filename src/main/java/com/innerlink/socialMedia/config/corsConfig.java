633869f44d4facf9d34be9d36cf0f43aa829fd5d
Nandani Hada <157029829+nandanihada@users.noreply.github.com>
2025-02-18 16:00:54 +0530

    save

--- src/main/java/com/innerlink/socialMedia/config/corsConfig.java
+++ src/main/java/com/innerlink/socialMedia/config/corsConfig.java
@@ -1,2 +1,21 @@
-package com.innerlink.socialMedia.config;public class corsConfig {
+package com.innerlink.socialMedia.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+
+public class corsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+
+}
 }
