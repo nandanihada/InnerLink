f52b2ee6e5383edd9c95800304799fccfa811c40
M4ster SINISTER <91175533+MasterSINISTER@users.noreply.github.com>
2025-02-21 12:11:22 +0530

    Updated

--- Frontend/innerlink/src/pages/Newsfeed.jsx
+++ Frontend/innerlink/src/pages/Newsfeed.jsx
@@ -1,29 +1,29 @@
-import React from 'react'
-import "../styles/Newsfeed.css"
-import Newscard from '../component/Newscard'
-import Footer from '../component/Footer'
+import React from "react";
+import "../styles/Newsfeed.css";
+import Newscard from "../component/Newscard";
+import Footer from "../component/Footer";
 function Newsfeed() {
   return (
     <>
-    
-    <div className="headin">
-    <img className='img' src="https://i.postimg.cc/52fHzDqJ/earth.gif" alt="Description of image"/>
+      <div className="headin">
+        <img
+          className="img"
+          src="https://i.postimg.cc/52fHzDqJ/earth.gif"
+          alt="Description of image"
+        />
         <h1> News</h1>
-      
-    </div>
-    <div className="together">
-        <Newscard/>
-    <Newscard/>
-    <Newscard/>
-    </div>
-   
-<footer>
-    <Footer/>
-</footer>
-  
-    
+      </div>
+      <div className="together">
+        <Newscard />
+        <Newscard />
+        <Newscard />
+      </div>
+
+      <footer>
+        <Footer />
+      </footer>
     </>
-  )
+  );
 }
 
-export default Newsfeed
+export default Newsfeed;
