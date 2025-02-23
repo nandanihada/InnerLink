c86cccc54e70c3ddb3c34a89c44b8338eff1eb15
M4ster SINISTER <91175533+MasterSINISTER@users.noreply.github.com>
2025-02-24 00:56:15 +0530

    Updated

--- src/main/java/com/innerlink/socialMedia/config/corsConfig.java
+++ src/main/java/com/innerlink/socialMedia/config/corsConfig.java
@@ -5,19 +5,38 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
+
 @Configuration
-public class corsConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // Sab endpoints ke liye
-                        .allowedOrigins("http://localhost:3000", "http://192.168.1.116:3000","https://inner-link.vercel.app/") // Allow frontend origin
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed methods
-                        .allowedHeaders("*") // Sab headers allow karo
-                        .allowCredentials(true); // Cookies aur authentication allow karo
-            }
-        };
+
+public class corsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000","https://inner-link.vercel.app/")
+                .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+
     }
 }
+
+//@Configuration
+//public class corsConfig {
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**") // Sab endpoints ke liye
+//                        .allowedOrigins("http://localhost:3000", "http://192.168.1.116:3000","https://inner-link.vercel.app/") // Allow frontend origin
+//                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed methods
+//                        .allowedHeaders("*") // Sab headers allow karo
+//                        .allowCredentials(true); // Cookies aur authentication allow karo
+//            }
+//        };
+//    }
+//}
+
+
+
